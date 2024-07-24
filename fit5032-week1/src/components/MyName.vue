5393b8732892de538b587186777699e112ab7f27
Kinleoapple <kinleoapple2460@gmail.com>
2024-07-24 13:14:40 +1000

    Modfiy the page to show my name.

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="myName">Hello, 
+        <span>Haojun</span>. 
+        This is your first vue app.</div>
+</template>
+
+<style>
+.myName {
+    margin-top: 5rem;
+    font-size: 1.2rem;
+}
+
+.myName span {
+    text-decoration: underline;
+    color: hsla(160, 100%, 37%, 1);
+}
+</style>
