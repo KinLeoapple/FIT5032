2d886608dbb1a904c9f15d358f99e2ca439d3f5e
hhua0083 <hhua0083@student.monash.edu>
2024-10-04 08:52:55 +1000

    Week 9

---
+++ hjhuang-library/src/views/GetBookCountView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import axios from "axios";
+import {ref} from "vue";
+
+const count = ref(null);
+const error = ref(null);
+
+const getBookCount = async () => {
+  try {
+    const response = await axios.get()
+  } catch (error) {
+
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <h1>Book Counter</h1>
+    <button @click="getBookCount">Get Book Count</button>
+    <p v-if="count !== null">Total number of books: {{ count }}</p>
+    <p v-if="error">{{ error }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
