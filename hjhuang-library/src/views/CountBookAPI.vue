0e1d6e98a5a76e4edc7677d616593ceba4775141
hhua0083 <hhua0083@student.monash.edu>
2024-10-04 21:41:20 +1000

    Week 10

--- hjhuang-library/src/views/CountBookAPI.vue
+++ hjhuang-library/src/views/CountBookAPI.vue
@@ -1,9 +1,27 @@
 <script setup>
+import axios from "axios";
+import {ref} from "vue";
 
+const jsondata = ref(null);
+const error = ref(null)
+
+const getBookCountAPI = async () => {
+  try {
+    const response = await axios.get("https://countbooks-kc5ola4uga-uc.a.run.app");
+    jsondata.value = response.data;
+    error.value = null;
+  } catch (error) {
+    console.error("Error fetching book count:", error);
+    error.value = error;
+    jsondata.value = null;
+  }
+}
+
+getBookCountAPI();
 </script>
 
 <template>
-
+  <pre>{{ jsondata }}</pre>
 </template>
 
 <style scoped>
