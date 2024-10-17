7ed1675cbf9f87a6de6e16e4add58114b8a6e27b
Kinleoapple <kinleoapple2460@gmail.com>
2024-10-17 13:53:00 +1100

    week 11

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -21,7 +21,7 @@ export default {
     },
     iconUrl() {
       return this.weatherData
-          ? `http://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
+          ? `https://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
           : null;
     },
   },
@@ -33,7 +33,7 @@ export default {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(async (position) => {
           const {latitude, longitude} = position.coords;
-          const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
           await this.fetchWeatherData(url);
         });
       }
