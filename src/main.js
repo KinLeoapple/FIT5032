5fbee1d865357378108effe22fe9a9e00a58a6c3
Kinleoapple <kinleoapple2460@gmail.com>
2024-10-17 13:13:25 +1100

    week 11

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
